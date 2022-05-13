16360a9d817234ca10d0b841c94a0137e8e2e2e7
Seven <52610264+seven102161@users.noreply.github.com>
2022-05-14 00:24:04 +0800

    design a polls page

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = [ '*' ]
 # Application definition
 
 INSTALLED_APPS = [
+    # customized apps
+    'polls.apps.PollsConfig',
+    # original apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
